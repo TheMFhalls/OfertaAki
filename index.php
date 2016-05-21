4c7326693fe0b480cd82e83d4683747c83894f8a
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2016-05-21 15:57:44 -0300

    update 21/05/2016

--- index.php
+++ index.php
@@ -5,20 +5,22 @@
 	include_once $_SERVER['DOCUMENT_ROOT'].'/OfertaAki/includes/head.php';
 ?>
 <body>
-	<div class="container bodyGeral bodyGeral-home">	
+	<div class="container bodyGeral bodyGeral-home">
+		<div class="topGeral topGeral-home">
+			<div class="row">
+				<?php
+				include_once $_SERVER['DOCUMENT_ROOT'].'/OfertaAki/includes/topGeral.php';
+				?>
+			</div>
+		</div>
 		<div class="content content-home">
 			<div class="row">
-				<label for="cidade" class="cidateLabel col-sm-6 text-center">
-					Informe sua Cidade :
-				</label>
-				<input class="cidadeClass col-sm-6 padding0px" autocomplete="off" id="cidadeId" type="text" 
-				name="cidade" onkeyup="javascript:getCidade();" onblur="javascript:setReturnCidade();" />
-				<div class="returnCidade padding0px col-sm-offset-6 col-sm-6" id="returnCidade">					
+				<div class="row">
+					<?php
+						include_once $_SERVER['DOCUMENT_ROOT'].'/OfertaAki/includes/content.php';
+					?>
 				</div>
 			</div>
-			<?php
-				@print_r($_SESSION);
-			?>
 		</div>
 	</div>
 </body>
