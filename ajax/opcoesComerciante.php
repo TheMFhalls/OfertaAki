b34de8b74df55699ad2a1abefda160f0be11415d
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2016-05-21 17:54:16 -0300

    update 21/05/2016

---
+++ ajax/opcoesComerciante.php
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dolorem
+dolorum eaque est facere fugiat ipsam libero magnam minus neque, non nostrum nulla
+omnis quam repellendus sed tempore veniam voluptates.
--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function setCidade(id_cid){
 function introdutionHome(){
 	var raiz = location.origin;
 	$.ajax({
-		url: raiz+'/OfertaAki/ajax/opcoesComerciante.html',
+		url: raiz+'/OfertaAki/ajax/opcoesComerciante.php',
 		type: 'GET',
 		success: function(res) {
 			var headline = $(res.responseText).text();
