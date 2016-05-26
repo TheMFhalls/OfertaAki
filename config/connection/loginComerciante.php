9a4d6e3426c048006fa7287f9b47e107fc840e63
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2016-05-26 12:00:47 -0300

    update 26/05/2016

--- config/connection/loginComerciante.php
+++ config/connection/loginComerciante.php
@@ -1,8 +1,11 @@
 <?php
     extract($_GET);
     include_once $_SERVER['DOCUMENT_ROOT'].'/OfertaAki/class/loginComerciante.class.php';
-    $comerciante = new loginComerciante($email_com, $senha_com);
-    if($comerciante->gerar()){
+    include_once $_SERVER['DOCUMENT_ROOT'].'/OfertaAki/class/protect.class.php';
+
+    $_SESSION['comerciante'] = new loginComerciante($email_com, $senha_com);
+
+    if($_SESSION['comerciante']->gerar()){
         header('location:../../main.php');
     }else{
 
