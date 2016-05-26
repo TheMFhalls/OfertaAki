4f49093cccfbd23a26c7a80c2d50c308b46b2df0
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2016-05-26 11:39:45 -0300

    update 26/05/2016

--- class/loginComerciante.class.php
+++ class/loginComerciante.class.php
@@ -11,7 +11,6 @@ class loginComerciante extends config {
 	private $email_com;
 	private $nomeFicticio_com;
 	private $id_cid_com;
-	private $id_tel_com;
 	private $horarioInicio_com;
 	private $horarioFinal_com;
 	private $razaoSocial_com;
@@ -33,26 +32,35 @@ class loginComerciante extends config {
 			email_com = '".$this->email_com."' AND
 			senha_com = '".$this->senha_com."'
 		";
-		$busca = $this->connection->getConnection();
-		$busca = $busca->query($query);
-		$busca = $busca->fetchAll();
-		return $busca[0];
+		$busca1 = $this->connection->getConnection();
+		$busca1 = $busca1->query($query);
+		$busca = $busca1->fetchAll();
+
+		if($busca1->rowCount() == 1){
+			return $busca[0];
+		}else{
+			return false;
+		}
+
 	}
 
 	public function gerar(){
-		$comerciante = $this->busca_com();
-		$this->cnpj_com = $comerciante['cnpj_com'];
-		$this->senha_com = $comerciante['senha_com'];
-		$this->email_com = $comerciante['email_com'];
-		$this->nomeFicticio_com = $comerciante['nomeFicticio_com'];
-		$this->id_cid_com = $comerciante['id_cid_com'];
-		$this->id_tel_com = $comerciante['id_tel_com'];
-		$this->horarioInicio_com = $comerciante['horarioInicio_com'];
-		$this->horarioFinal_com = $comerciante['horarioFinal_com'];
-		$this->razaoSocial_com = $comerciante['razaoSocial_com'];
-		$this->responsavel_com = $comerciante['responsavel_com'];
-		$this->bairro_com = $comerciante['bairro_com'];
-		$this->endereco_com = $comerciante['endereco_com'];
+		if($comerciante = $this->busca_com()){			;
+			$this->cnpj_com 			= $comerciante['cnpj_com'];
+			$this->senha_com 			= $comerciante['senha_com'];
+			$this->email_com 			= $comerciante['email_com'];
+			$this->nomeFicticio_com 	= $comerciante['nomeFicticio_com'];
+			$this->id_cid_com 			= $comerciante['id_cid_com'];
+			$this->horarioInicio_com 	= $comerciante['horarioInicio_com'];
+			$this->horarioFinal_com 	= $comerciante['horarioFinal_com'];
+			$this->razaoSocial_com 		= $comerciante['razaoSocial_com'];
+			$this->responsavel_com 		= $comerciante['responsavel_com'];
+			$this->bairro_com 			= $comerciante['bairro_com'];
+			$this->endereco_com 		= $comerciante['endereco_com'];
+		}else{
+			echo "Não conectado";
+		}
+		print_r($this);
 	}
 
 	public function setConnection(){
