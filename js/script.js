0eafaeddfbcdfdb45a7768175e2b17e1c6d99df4
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2016-05-30 08:18:16 -0300

    update 30/05/2016

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ function verificaComerciante(email_com){
 			dataType : "json",
 			url      : raiz+"/OfertaAki/ajax/verificaComerciante.php",
 			data     : { email_com : email_com},
-			//async    : true,
+			async    : true,
 			success  : function(resp){
 				if(resp.verificaComerciante == 'cadastrado'){
 					alert(true);
@@ -102,43 +102,56 @@ function validaCadastroComerciante(){
 			alert("Informe seu CNPJ!");
 			$('#cnpj_com').val("");
 			$('#cnpj_com').focus();
+			return false;
 		}else if($('#email_com').val()==''){
 			alert("Informe seu EMAIL!");
 			$('#email_com').val("");
 			$('#email_com').focus();
+			return false;
 		}else if($('#senha_com').val()==''){
 			alert("Informe sua SENHA!");
 			$('#senha_com').val("");
 			$('#senha_com').focus();
+			return false;
 		}else if($('#senha_com2').val()!=
 			$('#senha_com').val()){
 			alert("Senhas informadas são invalidas!");
 			$('#senha_com2').val("");
 			$('#senha_com2').focus();
+			return false;
 		}else if($('#id_cid_com').val()==''){
 			alert("Informe sua CIDADE!");
 			$('#id_cid_com2').val("");
 			$('#id_cid_com2').focus();
+			return false;
 		}else if($('#razaoSocial_com').val()==''){
 			alert("Informe sua RAZÃO SOCIAL!");
 			$('#razaoSocial_com').val("");
 			$('#razaoSocial_com').focus();
+			return false;
 		}else if($('#responsavel_com').val()==''){
 			alert("Informe o RESPONSAVEL pela Empresa!");
 			$('#responsavel_com').val("");
 			$('#responsavel_com').focus();
+			return false;
 		}else if($('#bairro_com').val()==''){
 			alert("Informe seu BAIRRO!");
 			$('#bairro_com').val("");
 			$('#bairro_com').focus();
+			return false;
 		}else if($('#endereco_com').val()==''){
 			alert("Informe seu ENDEREÇO!");
 			$('#endereco_com').val("");
 			$('#endereco_com').focus();
-		}else if(verificaComerciante($('#email_com').val())){
-			alert("EMAIL '"+$('#email_com').val()+"' já cadastrado, favor informe outro EMAIL!");
-			$('#email_com').val("");
-			$('#email_com').focus();
+			return false;
+		}else{
+			var teste = verificaComerciante($('#email_com').val());
+			if(teste){
+				alert("EMAIL '"+$('#email_com').val()+"' já cadastrado, favor informe outro EMAIL!");
+				$('#email_com').val("");
+				$('#email_com').focus();
+				return false;
+			}
 		}
 	}catch($e){
 		alert('Erro na validação do cadastro!');
