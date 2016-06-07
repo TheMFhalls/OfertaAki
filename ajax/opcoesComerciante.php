2c2bcfbed5a57917fd75bac087cf3268ad2d100b
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2016-06-06 21:46:03 -0300

    update 06/06/2016

--- ajax/opcoesComerciante.php
+++
@@ -1,3 +0,0 @@
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dolorem
-dolorum eaque est facere fugiat ipsam libero magnam minus neque, non nostrum nulla
-omnis quam repellendus sed tempore veniam voluptates.
